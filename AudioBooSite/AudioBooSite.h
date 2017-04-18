fedc7c197ee903247cc62147eae331bf0d1c6d7f
Alexander Shvets <oshvets@vonage.com>
2017-04-18 17:35:07 -0400

    Initial Commit

---
+++ AudioBooSite/AudioBooSite.h
@@ -0,0 +1,19 @@
+//
+//  AudioBooSite.h
+//  AudioBooSite
+//
+//  Created by Alexander Shvets on 4/18/17.
+//  Copyright © 2017 Alexander Shvets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioBooSite.
+FOUNDATION_EXPORT double AudioBooSiteVersionNumber;
+
+//! Project version string for AudioBooSite.
+FOUNDATION_EXPORT const unsigned char AudioBooSiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioBooSite/PublicHeader.h>
+
+
