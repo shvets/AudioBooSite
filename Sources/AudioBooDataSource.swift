d46d5f52afd7cefe00f3b40358daa8ebbe13b410
Alexander Shvets <alexander.shvets@gmail.com>
2017-04-26 22:52:21 -0400

    add site

--- Sources/AudioBooDataSource.swift
+++ Sources/AudioBooDataSource.swift
@@ -17,69 +17,42 @@ class AudioBooDataSource: DataSource {
       request = "Tracks"
     }
 
-//    switch request {
-//      case "Bookmarks":
-//        if let bookmarks = params.bookmarks {
-//          bookmarks.load()
-//          result = bookmarks.getBookmarks(pageSize: pageSize, page: currentPage)
-//        }
-//
-//      case "History":
-//        if let history = params.history {
-//          history.load()
-//          result = history.getHistoryItems(pageSize: pageSize, page: currentPage)
-//        }
-//
-//      case "Author":
-//        let path = selectedItem!.id
-//
-//        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
-//
-//      case "Performer":
-//        let path = selectedItem!.id
-//
-//        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
-//
-//      case "Genre Books":
-//        let path = selectedItem!.id
-//
-//        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
-//
-//      case "New Books":
-//        result = try service.getNewBooks(page: currentPage)["movies"] as! [Any]
-//
-//      case "Best Books":
-//        var period = "all"
-//
-//        if selectedItem!.name == "By Month" {
-//          period = "30"
-//        }
-//        else if selectedItem!.name == "By Week" {
-//          period = "7"
-//        }
-//
-//        result = try service.getBestBooks(period: period, page: currentPage)["movies"] as! [Any]
-//
-//      case "All Authors":
-//        result = try service.getAuthors(page: currentPage)["movies"] as! [Any]
-//
-//      case "Authors Letters":
-//        let letters = getLetters(AudioBooService.Authors)
-//
-//        var list = [Any]()
-//
+    switch request {
+      case "Bookmarks":
+        if let bookmarks = params.bookmarks {
+          bookmarks.load()
+          result = bookmarks.getBookmarks(pageSize: pageSize, page: currentPage)
+        }
+
+      case "History":
+        if let history = params.history {
+          history.load()
+          result = history.getHistoryItems(pageSize: pageSize, page: currentPage)
+        }
+
+      case "Author":
+        let path = selectedItem!.id
+
+        //result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+        result = try service.getBooks(path!) as! [Any]
+
+      case "Authors Letters":
+        //let letters = getLetters(AudioBooService.Authors)
+
+        var list = [Any]()
+
 //        list.append(["name": "Все"])
 //
 //        for letter in letters {
 //          list.append(["name": letter])
 //        }
-//
-//        result = list
-//
-//      case "Authors Letter Groups":
-//        if let letter = params.identifier {
-//          var letterGroups = [Any]()
-//
+
+        result = list
+
+      case "Authors Letter Groups":
+        if let letter = params.identifier {
+          var letterGroups = [Any]()
+
 //          for author in AudioBooService.Authors {
 //            let groupName = author.key
 //            let group = author.value
@@ -94,75 +67,29 @@ class AudioBooDataSource: DataSource {
 //              letterGroups.append(["name": groupName, "items": newGroup])
 //            }
 //          }
-//
-//          result = letterGroups
-//        }
-//
-//      case "Performers Letters":
-//        let letters = getLetters(AudioBooService.Performers)
-//
-//        var list = [Any]()
-//
-//        list.append(["name": "Все"])
-//
-//        for letter in letters {
-//          list.append(["name": letter])
-//        }
-//
-//        result = list
-//
-//      case "Performers Letter Groups":
-//        if let letter = params.identifier {
-//          var letterGroups = [Any]()
-//
-//          for performer in AudioBooService.Performers {
-//            let groupName = performer.key
-//            let group = performer.value
-//
-//            if groupName[groupName.startIndex] == letter[groupName.startIndex] {
-//              var newGroup: [Any] = []
-//
-//              for el in group {
-//                newGroup.append(["id": el.id, "name": el.name])
-//              }
-//
-//              letterGroups.append(["name": groupName, "items": newGroup])
-//            }
-//          }
-//
-//          result = letterGroups
-//        }
-//
-//      case "Group Authors":
-//        result = (selectedItem as! AudioBooMediaItem).items
-//
-//      case "All Performers":
-//        result = try service.getPerformers(page: currentPage)["movies"] as! [Any]
-//
-//      case "Group Performers":
-//        result = (selectedItem as! AudioBooMediaItem).items
-//
-//      case "Genres":
-//        result = try service.getGenres(page: currentPage)["movies"] as! [Any]
-//
-//      case "Tracks":
-//        let url = selectedItem!.id!
-//
-//        result = try service.getAudioTracks(url)
-//
-//      case "Search":
-//        if let identifier = params.identifier {
-//          if !identifier.isEmpty {
-//            result = try service.search(identifier, page: currentPage)["movies"] as! [Any]
-//          }
-//          else {
-//            result = []
-//          }
-//        }
-//
-//      default:
-//        result = []
-//    }
+
+          result = letterGroups
+        }
+
+
+      case "Tracks":
+        let url = selectedItem!.id!
+
+        result = try service.getAudioTracks(url)
+
+      case "Search":
+        if let identifier = params.identifier {
+          if !identifier.isEmpty {
+            result = try service.search(identifier, page: currentPage)
+          }
+          else {
+            result = []
+          }
+        }
+
+      default:
+        result = []
+    }
 
     if convert {
       return convertToMediaItems(result)
