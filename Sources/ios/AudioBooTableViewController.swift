2a97f9d8d40e05d41d8cb71beb2e406a8a4cbbab
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-07 20:00:01 -0400

    relax requirements to items

--- Sources/ios/AudioBooTableViewController.swift
+++ Sources/ios/AudioBooTableViewController.swift
@@ -28,7 +28,7 @@ open class AudioBooTableViewController: AudioBooBaseTableViewController {
   }
 
   override open func navigate(from view: UITableViewCell) {
-    let mediaItem = getItem(for: view)
+    let mediaItem = getItem(for: view) as! MediaItem
 
     switch mediaItem.name! {
       case "Authors":
@@ -60,7 +60,7 @@ open class AudioBooTableViewController: AudioBooBaseTableViewController {
           if let destination = segue.destination.getActionController() as? MediaItemsController,
              let view = sender as? MediaNameTableCell {
 
-            let mediaItem = getItem(for: view)
+            let mediaItem = getItem(for: view) as! MediaItem
 
             let adapter = AudioBooServiceAdapter(mobile: true)
 
