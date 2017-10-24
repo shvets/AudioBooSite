d27bc45a496fcfe566efc8a4a45192582dbaee00
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-24 14:26:35 -0400

    code improvement

--- Sources/AudioBooDataSource.swift
+++ Sources/AudioBooDataSource.swift
@@ -6,194 +6,202 @@ class AudioBooDataSource: DataSource {
   let service = AudioBooService.shared
 
   override open func load(params: Parameters) throws -> [Any] {
-    var result: [Any] = []
-    var tracks = false
+    var items: [Item] = []
 
     let selectedItem = params["selectedItem"] as? Item
 
     let request = params["requestType"] as! String
-    //let pageSize = params["pageSize"] as? Int
-    let currentPage = params["currentPage"] as? Int
+    let currentPage = params["currentPage"] as? Int ?? 1
 
     switch request {
     case "Bookmarks":
-      if let bookmarksManager = params["bookmarksManager"]  as? BookmarksManager,
+      if let bookmarksManager = params["bookmarksManager"] as? BookmarksManager,
          let bookmarks = bookmarksManager.bookmarks {
-        result = bookmarks.getBookmarks(pageSize: 60, page: currentPage!)
+        let data = bookmarks.getBookmarks(pageSize: 60, page: currentPage)
+
+        items = adjustItems(data)
       }
 
     case "History":
-      if let historyManager = params["historyManager"]  as? HistoryManager,
+      if let historyManager = params["historyManager"] as? HistoryManager,
          let history = historyManager.history {
-        result = history.getHistoryItems(pageSize: 60, page: currentPage!)
+        let data = history.getHistoryItems(pageSize: 60, page: currentPage)
+
+        items = adjustItems(data)
       }
 
     case "Authors Letters":
-      result = try service.getLetters()
+      items = adjustItems(try service.getLetters())
 
     case "Authors Letter Groups":
-      if let path = params["parentId"] as? String {
-        let authors = try service.getAuthorsByLetter(path)
-
-        for (key, value) in authors {
-          let group = value as! [NameClassifier.Item]
-
-          var newGroup: [[String: String]] = []
-
-          for el in group {
-            newGroup.append(["id": el.id, "name": el.name])
-          }
-
-          result.append(["name": key, "items": newGroup])
-        }
+      if let letter = params["parentId"] as? String {
+        items = adjustItems(try getAuthorsByLetter(letter))
       }
 
     case "Authors":
       if let selectedItem = selectedItem as? AudioBooMediaItem {
-        result = selectedItem.items
+        items = adjustItems(selectedItem.items)
       }
 
     case "Versions":
-      if let selectedItem = selectedItem {
-        let path = selectedItem.id
+      if let selectedItem = selectedItem,
+        let path = selectedItem.id as? String {
 
-        let playlistUrls = try service.getPlaylistUrls(path!)
+        let playlistUrls = try service.getPlaylistUrls(path)
 
-        var list = [[String: String]]()
-
-        for (index, url) in playlistUrls.enumerated() {
-          list.append(["name": "Version \(index+1)", "id": url])
+         for (index, url) in playlistUrls.enumerated() {
+          items.append(MediaItem(name: "Version \(index+1)", id: url))
         }
-
-        result = list
       }
 
     case "Author":
-      if let selectedItem = selectedItem {
-        result = try service.getBooks(selectedItem.id!)
+      if let selectedItem = selectedItem,
+         let id = selectedItem.id as? String {
+        items = adjustItems(try service.getBooks(id))
       }
 
     case "Tracks":
-      if let selectedItem = selectedItem {
+      if let selectedItem = selectedItem,
+         let id = selectedItem.id as? String {
+        let playlistUrls = try service.getPlaylistUrls(id)
+
         let version = params["version"] as? Int ?? 0
-        let playlistUrls = try service.getPlaylistUrls(selectedItem.id!)
 
         if playlistUrls.count > version {
           let url = playlistUrls[version]
 
-          tracks = true
-
-          result = try service.getAudioTracks(url)
+          items = adjustItems(try service.getAudioTracks(url))
         }
       }
 
     case "Search":
       if let query = params["query"] as? String {
         if !query.isEmpty {
-          result = try service.search(query, page: currentPage!)
-        }
-        else {
-          result = []
+           items = adjustItems(try service.search(query, page: currentPage))
         }
       }
 
     default:
-      result = []
+      items = []
     }
 
-    let convert = params["convert"] as? Bool ?? true
-
-    if convert || tracks {
-      return convertToMediaItems(result)
-    }
-    else {
-        return result 
-    }
+    return items
   }
 
-  func convertToMediaItems(_ items: Any) -> [Any] {
-    var newItems = [Any]()
+  func adjustItems(_ items: [Any]) -> [Item] {
+    var newItems = [Item]()
 
-    if let tracks = items as? [AudioBooAPI.BooTrack] {
-      for track in tracks {
-        let item = AudioItem(name: track.title + ".mp3", id: track.url)
-
-        newItems += [item]
-      }
-    }
-    else if let items = items as? [[String: Any]] {
-      for item in items {
-        let movie = AudioBooMediaItem(data: ["name": ""])
-
-        if let dict = item as? [String: String] {
-          movie.name = dict["name"]
-          movie.id = dict["id"]
-          movie.description = dict["description"]
-          movie.thumb = dict["thumb"]
-          movie.type = dict["type"]
-        }
-        else {
-          movie.name = item["name"] as? String
-
-          if let array = item["items"] as? [[String: String]] {
-            var newArray = [AudioBooAPI.PersonName]()
-
-            for elem in array {
-              let newElem = AudioBooAPI.PersonName(name: elem["name"]!, id: elem["id"]!)
-
-              newArray.append(newElem)
-            }
-
-            movie.items = newArray
-          }
-        }
-
-        newItems += [movie]
-      }
-    }
-    else if let items = items as? [AudioBooAPI.PersonName] {
-      for item in items {
-        let movie = AudioBooMediaItem(data: ["name": ""])
-
-        movie.name = item.name
-        movie.id = item.id
-
-        newItems += [movie]
-      }
-    }
-    else if let items = items as? [HistoryItem] {
-      for item in items {
-        let movie = AudioBooMediaItem(data: ["name": ""])
-
-        movie.name = item.item.name
-        movie.id = item.item.id
-        movie.description = item.item.description
-        movie.thumb = item.item.thumb
-        movie.type = item.item.type
-
-        newItems += [movie]
+    if let items = items as? [HistoryItem] {
+      newItems = transform(items) { item in
+        createHistoryItem(item as! HistoryItem)
       }
     }
     else if let items = items as? [BookmarkItem] {
-      for item in items {
-        let movie = AudioBooMediaItem(data: ["name": ""])
-
-        movie.name = item.item.name
-        movie.id = item.item.id
-        movie.description = item.item.description
-        movie.thumb = item.item.thumb
-        movie.type = item.item.type
-
-        newItems += [movie]
+      newItems = transform(items) { item in
+        createBookmarkItem(item as! BookmarkItem)
       }
+    }
+    else if let items = items as? [AudioBooAPI.PersonName] {
+      newItems = transform(items) { item in
+        let item = item as! AudioBooAPI.PersonName
+
+        return MediaItem(name: item.name, id: String(describing: item.id))
+      }
+    }
+    else if let items = items as? [AudioBooAPI.BooTrack] {
+      newItems = transform(items) { item in
+        let track = item as! AudioBooAPI.BooTrack
+
+        return MediaItem(name: track.title + ".mp3", id: String(describing: track.url))
+      }
+    }
+
+    else if let items = items as? [[String: Any]] {
+      newItems = transform(items) { item in
+        createMediaItem(item as! [String: Any])
+      }
+    } else if let items = items as? [Item] {
+      newItems = items
     }
 
     return newItems
   }
 
+  func createHistoryItem(_ item: HistoryItem) -> Item {
+    let newItem = MediaItem(data: ["name": ""])
+
+    newItem.name = item.item.name
+    newItem.id = item.item.id
+    newItem.description = item.item.description
+    newItem.thumb = item.item.thumb
+    newItem.type = item.item.type
+
+    return newItem
+  }
+
+  func createBookmarkItem(_ item: BookmarkItem) -> Item {
+    let newItem = MediaItem(data: ["name": ""])
+
+    newItem.name = item.item.name
+    newItem.id = item.item.id
+    newItem.description = item.item.description
+    newItem.thumb = item.item.thumb
+    newItem.type = item.item.type
+
+    return newItem
+  }
+
+  func createMediaItem(_ item: [String: Any]) -> Item {
+    let newItem = AudioBooMediaItem(data: ["name": ""])
+
+    if let dict = item as? [String: String] {
+      newItem.name = dict["name"]
+      newItem.id = dict["id"]
+      newItem.description = dict["description"]
+      newItem.thumb = dict["thumb"]
+      newItem.type = dict["type"]
+    } else {
+      newItem.name = item["name"] as? String
+
+      if let array = item["items"] as? [[String: String]] {
+        var newArray = [AudioBooAPI.PersonName]()
+
+        for elem in array {
+          let newElem = AudioBooAPI.PersonName(name: elem["name"]!, id: elem["id"]!)
+
+          newArray.append(newElem)
+        }
+
+        newItem.items = newArray
+      }
+    }
+
+    return newItem
+  }
+
+  func getAuthorsByLetter(_ letter: String) throws -> [[String: Any]] {
+    var data = [[String: Any]]()
+
+    let authors = try service.getAuthorsByLetter(letter)
+
+    for (key, value) in authors {
+      if let group = value as? [NameClassifier.Item] {
+        var newGroup: [[String: String]] = []
+
+        for el in group {
+          newGroup.append(["id": el.id, "name": el.name])
+        }
+
+        data.append(["name": key, "items": newGroup])
+      }
+    }
+
+    return data
+  }
+
   func getLetters(_ items: [NameClassifier.ItemsGroup]) -> [String] {
-    var rletters = [String]()
-    var eletters = [String]()
+    var ruLetters = [String]()
+    var enLetters = [String]()
 
     for item in items {
       let groupName = item.key
@@ -203,18 +211,18 @@ class AudioBooDataSource: DataSource {
       let letter = String(groupName[index])
 
       if (letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z") {
-        if !eletters.contains(letter) {
-          eletters.append(letter)
+        if !enLetters.contains(letter) {
+          enLetters.append(letter)
         }
       }
       else if (letter >= "а" && letter <= "я") || (letter >= "А" && letter <= "Я") {
-        if !rletters.contains(letter) {
-          rletters.append(letter)
+        if !ruLetters.contains(letter) {
+          ruLetters.append(letter)
         }
       }
     }
 
-    return rletters + eletters
+    return ruLetters + enLetters
   }
 
 }
